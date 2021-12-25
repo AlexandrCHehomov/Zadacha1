7e211ff91b40e73ba83370fa54b5c871380db0ac
AlexCH <alexvilah@mail.ru>
2021-12-25 13:30:23 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+byte a = 127;
+short b = 348;
+int c = 14522;
+long d = 4456686841L;
+float f = (float) 1.454785123E06;
+double m = 23458.1234;
+char l = 1238;
+boolean j = true;
+
+}
